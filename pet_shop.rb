06b2883efd0c94cd179fa4f6af1108495a65b346
Rob Wilson <rob.wilson195@gmail.com>
2018-10-19 14:31:47 +0100

    Finished non-optional tasks.

--- pet_shop.rb
+++ pet_shop.rb
@@ -25,9 +25,45 @@ end
 def pets_by_breed(shop, breed)
   breed_list = []
   for pet in shop[:pets]
-  if pet[:breed] == breed
-    breed_list << pet[:name]
+    if pet[:breed] == breed
+      breed_list << pet[:name]
+    end
+  end
+  return breed_list
+end
+
+def find_pet_by_name(shop, name)
+  for pet in shop[:pets]
+    return pet if pet[:name] == name
+  end
+  return nil
+end
+
+def remove_pet_by_name(shop, name)
+  for pet in shop[:pets]
+    if pet[:name] == name
+      pet_cage = shop[:pets].index(pet)
+      shop[:pets].delete_at(pet_cage)
+    end
   end
 end
-return breed_list
+
+def add_pet_to_stock(shop, new_pet)
+  shop[:pets] << new_pet
+end
+
+def customer_cash(client)
+  return client[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].length
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets] << new_pet
 end
