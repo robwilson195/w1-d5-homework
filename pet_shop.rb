57c823045ab4c909a91cb30afd121ac92ef68d94
Rob Wilson <rob.wilson195@gmail.com>
2018-10-19 13:06:59 +0100

    Finished up to increase pets sold.

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,19 @@
 def pet_shop_name(shop)
   return shop[:name]
 end
+
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, amount)
+  shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, num)
+  shop[:admin][:pets_sold] += num
+end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -73,10 +73,10 @@ class TestPetShop < Minitest::Test
     }
   end
 
-  def test_pet_shop_name
-    name = pet_shop_name(@pet_shop)
-    assert_equal("Camelot of Pets", name)
-  end
+  # def test_pet_shop_name
+  #   name = pet_shop_name(@pet_shop)
+  #   assert_equal("Camelot of Pets", name)
+  # end
 
   # def test_total_cash
   #   sum = total_cash(@pet_shop)
@@ -106,10 +106,10 @@ class TestPetShop < Minitest::Test
   #   assert_equal(2, sold)
   # end
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
